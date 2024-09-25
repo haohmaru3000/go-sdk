7679559a227589b91ad2d1a062272e9e4f203113
haohmaru3000 <tuanit168@gmail.com>
2024-09-25 18:09:58 +0700

    Update JWT plugin

---
+++ plugin/tokenprovider/jwt/jwt.go
@@ -0,0 +1,116 @@
+package jwt
+
+import (
+	"flag"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/haohmaru3000/go-sdk/plugin/tokenprovider"
+)
+
+type jwtProvider struct {
+	name   string
+	secret string
+}
+
+func NewJWTProvider(name string) *jwtProvider {
+	return &jwtProvider{name: name}
+}
+
+func (p *jwtProvider) GetPrefix() string {
+	return p.Name()
+}
+
+func (p *jwtProvider) Get() interface{} {
+	return p
+}
+
+func (p *jwtProvider) Name() string {
+	return p.name
+}
+
+func (p *jwtProvider) InitFlags() {
+	flag.StringVar(&p.secret, "jwt-secret", "200Lab.io", "Secret key for generating JWT")
+}
+
+func (p *jwtProvider) Configure() error {
+	return nil
+}
+
+func (p *jwtProvider) Run() error {
+	return nil
+}
+
+func (p *jwtProvider) Stop() <-chan bool {
+	c := make(chan bool)
+	go func() {
+		c <- true
+	}()
+	return c
+}
+
+func (j *jwtProvider) SecretKey() string {
+	return j.secret
+}
+
+func (j *jwtProvider) Generate(data tokenprovider.TokenPayload, expiry int) (tokenprovider.Token, error) {
+	// generate the JWT
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, myClaims{
+		data,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Local().Add(time.Second * time.Duration(expiry))),
+			IssuedAt:  jwt.NewNumericDate(time.Now().Local()),
+		},
+	})
+
+	myToken, err := t.SignedString([]byte(j.secret))
+	if err != nil {
+		return nil, err
+	}
+
+	// return the token
+	return &token{
+		Token:   myToken,
+		Expiry:  expiry,
+		Created: time.Now(),
+	}, nil
+}
+
+func (j *jwtProvider) Validate(myToken string) (tokenprovider.TokenPayload, error) {
+	res, err := jwt.ParseWithClaims(myToken, &myClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(j.secret), nil
+	})
+
+	if err != nil {
+		return nil, tokenprovider.ErrInvalidToken
+	}
+
+	// validate the token
+	if !res.Valid {
+		return nil, tokenprovider.ErrInvalidToken
+	}
+
+	claims, ok := res.Claims.(*myClaims)
+
+	if !ok {
+		return nil, tokenprovider.ErrInvalidToken
+	}
+
+	// return the token
+	return claims.Payload, nil
+}
+
+type myClaims struct {
+	Payload tokenprovider.TokenPayload `json:"payload"`
+	jwt.RegisteredClaims
+}
+
+type token struct {
+	Token   string    `json:"token"`
+	Created time.Time `json:"created"`
+	Expiry  int       `json:"expiry"`
+}
+
+func (t *token) GetToken() string {
+	return t.Token
+}
